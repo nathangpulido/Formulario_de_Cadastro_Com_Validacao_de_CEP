571f1ad51f14eb5cdf2803e37c07c179e6f57873
nathangpulido <nathang.pulido98@outlook.com>
2024-10-10 15:13:26 -0300

    Inclusão de arquivo js para consumir api de cep

---
+++ script.js
@@ -0,0 +1,19 @@
+async function buscarEndereco() {
+    const cep = document.getElementById('cep').value;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        if (data.erro) {
+            document.getElementById('cep').value = "CEP inválido.";
+        } else {
+            document.getElementById('endereco').value = data.logradouro;
+            document.getElementById('cidade').value = data.localidade;
+            document.getElementById('estado').value = data.uf;
+        }
+    } catch (error) {
+        document.getElementById('endereco').value = "Erro ao buscar o endereço.";
+    }
+}
